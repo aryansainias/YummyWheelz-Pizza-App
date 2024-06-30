62351bdb139c13b8a75c64b54b81a87783bea056
aryansainias <155607558+aryansainias@users.noreply.github.com>
2024-06-30 14:40:35 +0530

    Create footer.jsx

---
+++ src/components/Footer/footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { ListGroup } from "reactstrap";
+
+import logo from "../../assets/images/res-logo.png";
+import "../../styles/footer.css";
+
+const Footer = () => {
+  return (
+   
+    <footer/>
+  );
+};
+
+export default Footer;
