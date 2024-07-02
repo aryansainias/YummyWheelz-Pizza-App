1f8a31175795be239b6cdfd07fa4565f833c14e3
Aryan Saini <155607558+aryansainias@users.noreply.github.com>
2024-07-02 13:39:53 +0530

    Create App.js

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css'; // We'll create this CSS file later
+import Footer from './Footer/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to Yummy Wheelz</h1>
+      </header>
+      <main>
+        <p>Your food delivery app content goes here!</p>
+      </main>
+      
+    </div>
+  );
+}
+
+export default App;
